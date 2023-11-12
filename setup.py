4d0bb4743f7ec787d1ad7fc8119e443c6d2877b0
Vamsi Thiriveedhi <115020590+vkt1414@users.noreply.github.com>
2023-11-12 18:02:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='idc_index',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     include_package_data=True,  
     install_requires=['pandas', 'requests', 'logging'],
