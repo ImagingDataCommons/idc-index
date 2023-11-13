ef9a93a043013160d63e1ffd9dcc831d910686df
Vamsi Thiriveedhi <115020590+vkt1414@users.noreply.github.com>
2023-11-13 16:47:45 -0500

    bumping for gdit testing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='idc_index',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,  
     install_requires=['pandas', 'requests'],
