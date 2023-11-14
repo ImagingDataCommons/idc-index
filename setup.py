d7613a9e4f09b8932b152954c8572e26d8f41831
vkt1414 <vthiriveedhi@mgh.harvard.edu>
2023-11-14 15:28:52 -0500

    bumping up 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='idc_index',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,  
     install_requires=['pandas', 'requests'],
