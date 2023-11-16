c2ad56cc6b0b8fcce86bfa2e6d600c5415250cb0
Vamsi Thiriveedhi <115020590+vkt1414@users.noreply.github.com>
2023-11-16 12:29:24 -0500

    bump up for collection_summary in IDCClient

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class PostInstallCommand(install):
                 logging.error('Failed to download s5cmd:', e)
 setup(
     name='idc_index',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['pandas', 'requests'],
