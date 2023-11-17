295c567a1b8e4b0e5c9fe4274283346e84b5264e
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-11-17 13:21:45 -0500

    update release to 0.2.4

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class PostInstallCommand(install):
                 logging.error('Failed to download s5cmd:', e)
 setup(
     name='idc_index',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['pandas', 'requests'],
