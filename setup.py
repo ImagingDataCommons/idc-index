8c02b2bc8348619eefd8b58043dc1a411aeae2cc
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-11-21 15:17:28 -0500

    release 0.2.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import urllib.request
 import tarfile
 import zipfile
 
-package_version = "0.2.5"
+package_version = "0.2.6"
 
 # Read long description from README
 with open("README.md", "r", encoding="utf-8") as fh:
