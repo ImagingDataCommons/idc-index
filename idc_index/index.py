2f81497acf1d5385488f3f7c84b048ffa0221c4d
ds_93 <daniela.schacherer@web.de>
2024-10-01 11:39:21 +0200

    added index_crdc_series_uuid

--- idc_index/index.py
+++ idc_index/index.py
@@ -1607,6 +1607,7 @@ Destination folder is not empty and sync size is less than total size.
                 SELECT
                     series_aws_url,
                     CONCAT(TRIM('*' FROM series_aws_url), crdc_instance_uuid, '.dcm') as instance_url,
+                    REGEXP_EXTRACT(series_aws_url, '(?:.*?\\/){{3}}([^\\/?#]+)', 1) index_crdc_series_uuid,
                     series_size_MB,
                     {hierarchy} as path
                 FROM
